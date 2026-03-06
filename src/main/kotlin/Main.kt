7d263d9811790f7b4591ff5064017cd57ea332e4
aaron050223 <agallardoc@iesrafaelalberti.es>
2026-03-06 18:21:04 +0100

    Edito el Main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,12 +4,12 @@ import org.iesra.revilofe.Pila
 
 fun main(){
     val pila: Pila<String> = Pila<String>()
-    pila.push("one")
-    pila.push("two")
-    pila.push("three")
-    pila.push("four")
+    pila.push("1")
+    pila.push("2")
+    pila.push("3")
+    pila.push("4")
     val numbersRev = reverse(pila.pila)
-    if (listOf("four", "three", "two", "one") != numbersRev)
+    if (listOf("4", "3", "2", "1") != numbersRev)
         println("Error")
     else
         println("Correcto")
