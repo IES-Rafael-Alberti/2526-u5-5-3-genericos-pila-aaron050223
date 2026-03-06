2d0b6b14df59934a697a7dadedf76f085a76a1bc
aaron050223 <agallardoc@iesrafaelalberti.es>
2026-03-06 18:01:57 +0100

    Añado el PilaIterator

--- src/main/kotlin/PilaIterator.kt
+++ src/main/kotlin/PilaIterator.kt
@@ -3,13 +3,11 @@ package org.iesra.revilofe
 import kotlin.collections.Iterator
 
 class PilaIterator<T>(val pila: MutableList<T>): Iterator<T> {
-
     override fun next(): T {
         val next = pila.last()
         pila.removeLast()
         return next
     }
-
 
     override fun hasNext(): Boolean {
         return if (pila.isNotEmpty()) {
