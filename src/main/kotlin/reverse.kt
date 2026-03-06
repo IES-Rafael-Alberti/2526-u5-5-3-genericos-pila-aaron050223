836f7dc8dda58b829bedb640d3b47576ee5365d1
aaron050223 <agallardoc@iesrafaelalberti.es>
2026-03-06 18:17:14 +0100

    Añado el Reverse

---
+++ src/main/kotlin/reverse.kt
@@ -0,0 +1,9 @@
+package org.iesra.revilofe
+
+fun <T> reverse(lista: List<T>): MutableList<T>{
+    val listaInvertida: MutableList<T> = mutableListOf<T>()
+    for (i in lista.size-1 downTo 0){
+        listaInvertida.add(lista[i])
+    }
+    return listaInvertida
+}
